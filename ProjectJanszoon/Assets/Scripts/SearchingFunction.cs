73bd8c00ff4766bceca2e9338723d2f43aaf607f
William DK <116037580+WilliamDK64@users.noreply.github.com>
2023-09-19 18:39:05 +1200

    19/9
    Today I completed the colour searching parameter, so the searching function is now entirely complete and modular. I began creating a separate page where the birds will appear, but until that is fully implemented the search results will appear beneath the parameter buttons.

--- ProjectJanszoon/Assets/Scripts/SearchingFunction.cs
+++ ProjectJanszoon/Assets/Scripts/SearchingFunction.cs
@@ -1,4 +1,5 @@
 using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,21 +16,34 @@ public class SearchingFunction : MonoBehaviour
     [SerializeField] private Transform _canvas;
     public void Search()
     {
+        // Create a list so that Birds array is unchanged
         List<GameObject> birdList = new();
         birdList.AddRange(Birds);
 
         foreach(GameObject bird in Birds)
         {
             Bird birdScript = bird.GetComponent<Bird>();
-            if(birdScript.IsFlying != IsFlying) 
+
+            // Flying?
+            // If the flying parameter has been set but the bird is not what the parameter is set to, remove the bird.
+            if(IsFlying != null && birdScript.IsFlying != IsFlying) 
             {
                 birdList.Remove(bird);
+            }
+            
+            // Colors?
+            foreach(string c in Colors) 
+            {
+                // If the color isn't empty and there is no element of that color on the bird, remove the bird.
+                if(c != "" && Array.Exists(birdScript.Colors, element => element == c) == false)
+                {
+                    birdList.Remove(bird);
+                }
             }
         }
         
         foreach(GameObject bird in birdList)
         {
-            Debug.Log(bird);
             Instantiate(bird, new Vector2(0, 0), Quaternion.identity, _canvas);
         }
     }
